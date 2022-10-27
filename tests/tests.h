848284d5b94efbaa900b058588ad0831147e28ba
assassin <assassin@student.21-school.ru>
2022-10-27 13:05:46 +0300

    final

---
+++ tests/tests.h
@@ -0,0 +1,26 @@
+#ifndef TEST
+#define TEST
+
+#include <check.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "../c_calc_polish.h"
+
+Suite *suite_s21_eq_matrix(void);
+Suite *suite_s21_create_matrix(void);
+Suite *suite_s21_sum_matrix(void);
+Suite *suite_s21_sub_matrix(void);
+Suite *suite_s21_mult_number_matrix(void);
+Suite *suite_s21_mult_matrix(void);
+Suite *suite_s21_transpose_matrix(void);
+Suite *suite_s21_calc_complements(void);
+Suite *suite_s21_determinant(void);
+Suite *suite_s21_inverse_matrix(void);
+
+void run_tests(void);
+void run_testcase(Suite *testcase);
+
+double get_rand(double min, double max);
+#endif  //  TEST
